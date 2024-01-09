aea7a47141c3cd0799f5d21bf07b3c38b98e9831
Sangit Manandhar <rux12@icloud.com>
2024-01-09 15:59:38 -0500

    chore : change rust lsp

--- .config/nvim/lua/enix/lsp.lua
+++ .config/nvim/lua/enix/lsp.lua
@@ -166,17 +166,15 @@ require("lspconfig").gopls.setup(
 )
 
 require("lspconfig").rust_analyzer.setup(
-    config(
-    --[[
+    config({
         settings = {
-            rust = {
-                unstable_features = true,
-                build_on_save = false,
-                all_features = true,
-            },
+            ["rust-analyzer"] = {
+                checkOnSave = {
+                    command = "clippy"
+                }
+            }
         }
-    --]]
-    )
+    })
 )
 
 
